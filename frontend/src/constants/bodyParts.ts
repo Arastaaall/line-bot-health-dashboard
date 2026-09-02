97a196cd6e2ec36320bcd4be0b71b7ec08e647da
Arastaaall <watatia1125@gmail.com>
2026-09-03 01:51:04 +0900

    fix: add bodyParts constants

---
+++ frontend/src/constants/bodyParts.ts
@@ -0,0 +1,12 @@
+export const BODY_PART_ORDER = ['chest', 'back', 'legs', 'shoulder', 'arms', 'full_body', 'cardio', 'other'];
+
+export const BODY_PART_LABELS: Record<string, string> = {
+  chest: '胸',
+  back: '背中',
+  legs: '脚',
+  shoulder: '肩',
+  arms: '腕',
+  full_body: '全身',
+  cardio: '有酸素',
+  other: 'その他',
+};
